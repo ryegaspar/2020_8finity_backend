b1c2448c59d940821070c62dd1f7f7fe71963fb2
rye gaspar <gaspar.ryan@outlook.com>
2020-08-01 19:51:48 -0500

    add test - money test

---
+++ app/ExpenseTracker/Money.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\ExpenseTracker;
+
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Money\Formatter\IntlMoneyFormatter;
+use Money\Money as BaseMoney;
+use NumberFormatter;
+
+class Money
+{
+    protected $money;
+
+    /**
+     * Money constructor.
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->money = new BaseMoney($value, new Currency('PHP'));
+    }
+
+    public function amount()
+    {
+        return $this->money->getAmount();
+    }
+
+    public function formatted()
+    {
+        $formatter = new IntlMoneyFormatter(
+            new NumberFormatter('fil_PH', NumberFormatter::CURRENCY),
+            new ISOCurrencies()
+        );
+
+        return $formatter->format($this->money);
+    }
+
+    public function add(Money $money)
+    {
+        $this->money = $this->money->add($money->instance());
+
+        return $this;
+    }
+
+    public function instance()
+    {
+        return $this->money;
+    }
+}
