eda2d0e30df5e97cba441f740e6eaafc91bc9731
rye gaspar <gaspar.ryan@outlook.com>
2021-02-08 21:52:42 -0600

    transactions - add test - sort, filter and search

--- tests/Feature/Admin/Transactions/getTransactionsTest.php
+++ tests/Feature/Admin/Transactions/getTransactionsTest.php
@@ -14,88 +14,229 @@ class getTransactionsTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $transactionDay1;
+    protected $transactionDay2;
+    protected $categoryIncome;
+    protected $categoryExpense;
+    protected $transaction1;
+    protected $transaction2;
+    protected $transaction3;
+    protected $admin;
+
     /** @test */
-    public function can_view_transactions()
+    public function can_view_transactions_and_is_ordered_by_date_by_default()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id'   => $this->transaction1->id,
+                        'date' => $this->transactionDay1,
+                    ],
+                    [
+                        'id'   => $this->transaction2->id,
+                        'date' => $this->transactionDay1,
+                    ],
+                    [
+                        'id'   => $this->transaction3->id,
+                        'date' => $this->transactionDay2,
+                    ],
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function has_paginated_data()
     {
         $admin = Admin::factory()->create();
 
-        $transactionDay1 = Carbon::parse("first day of this month")->format("Y-m-d");
-        $transactionDay2 = Carbon::parse("first day of this month")->addDay()->format("Y-m-d");
-        $categoryIncome = Category::factory()->income()->create();
-        $categoryExpense = Category::factory()->expense()->create();
-
-        $transaction1 = Transaction::create([
-            'category_id' => $categoryIncome->id,
-            'admin_id'    => $admin->id,
-            'amount'      => 12000,
-            'date'        => $transactionDay1
-        ]);
-
-        $transaction2 = Transaction::create([
-            'category_id' => $categoryExpense->id,
-            'admin_id'    => $admin->id,
-            'amount'      => 8000,
-            'date'        => $transactionDay1
-        ]);
-
-        $transaction3 = Transaction::create([
-            'category_id' => $categoryIncome->id,
-            'admin_id'    => $admin->id,
-            'amount'      => 7000,
-            'date'        => $transactionDay2
+        Transaction::factory()->create([
+            'admin_id' => $admin->id
         ]);
 
         $this->actingAs($admin, 'admin')
             ->withHeaders(['accept' => 'application/json'])
             ->getJson('admin/transactions')
+            ->assertJsonStructure([
+                'total',
+                'per_page',
+                'current_page',
+                'last_page',
+                'next_page_url',
+                'prev_page_url',
+                'from',
+                'to',
+                'data',
+            ]);
+    }
+
+    /** @test */
+    public function can_sort_by_amount()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions?sort=amount|asc')
             ->assertJson([
-                'data'         => [
+                'data' => [
                     [
-                        'id'               => $transaction1->id,
-                        'description'      => $transaction1->description,
-                        'notes'            => $transaction1->notes,
-                        'amount'           => "12000",
-                        'amount_formatted' => "₱120.00",
-                        'date'             => $transactionDay1,
-                        'category_type'    => 'income',
-                        'category_icon'    => $categoryIncome->icon,
-                        'category_name'    => $categoryIncome->name,
-                        'category_id'      => "{$categoryIncome->id}",
-                        'admin_id'         => $admin->id,
-                        'admin_first_name' => $admin->first_name,
-                        'admin_last_name'  => $admin->last_name
+                        'id'     => $this->transaction3->id,
+                        'amount' => "7000",
                     ],
                     [
-                        'id'               => $transaction2->id,
-                        'description'      => $transaction2->description,
-                        'notes'            => $transaction2->notes,
-                        'amount'           => "8000",
-                        'amount_formatted' => "₱80.00",
-                        'date'             => $transactionDay1,
-                        'category_type'    => 'expense',
-                        'category_icon'    => $categoryExpense->icon,
-                        'category_name'    => $categoryExpense->name,
-                        'category_id'      => "{$categoryExpense->id}",
-                        'admin_id'         => $admin->id,
-                        'admin_first_name' => $admin->first_name,
-                        'admin_last_name'  => $admin->last_name
+                        'id'     => $this->transaction2->id,
+                        'amount' => "8000",
                     ],
                     [
-                        'id'               => $transaction3->id,
-                        'description'      => $transaction3->description,
-                        'notes'            => $transaction3->notes,
-                        'amount'           => "7000",
-                        'amount_formatted' => "₱70.00",
-                        'date'             => $transactionDay2,
-                        'category_type'    => 'income',
-                        'category_icon'    => $categoryIncome->icon,
-                        'category_name'    => $categoryIncome->name,
-                        'category_id'      => "{$categoryIncome->id}",
-                        'admin_id'         => $admin->id,
-                        'admin_first_name' => $admin->first_name,
-                        'admin_last_name'  => $admin->last_name
+                        'id'     => $this->transaction1->id,
+                        'amount' => "12000",
                     ],
                 ]
             ]);
     }
+
+    /** @test */
+    public function can_sort_by_date()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions?sort=date|asc')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id'   => $this->transaction1->id,
+                        'date' => $this->transactionDay1,
+                    ],
+                    [
+                        'id'   => $this->transaction2->id,
+                        'date' => $this->transactionDay1,
+                    ],
+                    [
+                        'id'   => $this->transaction3->id,
+                        'date' => $this->transactionDay2,
+                    ],
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function can_filter_income_only()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions?filter=income')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id' => $this->transaction1->id,
+                    ],
+                    [
+                        'id' => $this->transaction3->id,
+                    ],
+                ]
+            ])
+            ->assertJsonMissing([
+                'data' => [
+                    'id' => $this->transaction2->id
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function can_filter_expenses_only()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions?filter=expense')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id' => $this->transaction2->id,
+                    ],
+                ]
+            ])
+            ->assertJsonMissing([
+                'data' => [
+                    [
+                        'id' => $this->transaction1->id
+                    ],
+                    [
+                        'id' => $this->transaction3->id,
+                    ],
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function can_search_through_description()
+    {
+        $this->create_transactions();
+
+        $this->actingAs($this->admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/transactions?search=transaction2')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id' => $this->transaction2->id,
+                    ],
+                ]
+            ])
+            ->assertJsonMissing([
+                'data' => [
+                    [
+                        'id' => $this->transaction1->id
+                    ],
+                    [
+                        'id' => $this->transaction3->id,
+                    ],
+                ]
+            ]);
+    }
+
+    protected function create_transactions()
+    {
+        $this->admin = Admin::factory()->create();
+
+        $this->transactionDay1 = Carbon::parse("first day of this month")->format("Y-m-d");
+        $this->transactionDay2 = Carbon::parse("first day of this month")->addDay()->format("Y-m-d");
+        $this->categoryIncome = Category::factory()->income()->create();
+        $this->categoryExpense = Category::factory()->expense()->create();
+
+        $this->transaction1 = Transaction::create([
+            'category_id' => $this->categoryIncome->id,
+            'description' => 'transaction1',
+            'admin_id'    => $this->admin->id,
+            'amount'      => 12000,
+            'date'        => $this->transactionDay1
+        ]);
+
+        $this->transaction2 = Transaction::create([
+            'category_id' => $this->categoryExpense->id,
+            'description' => 'transaction2',
+            'admin_id'    => $this->admin->id,
+            'amount'      => 8000,
+            'date'        => $this->transactionDay1
+        ]);
+
+        $this->transaction3 = Transaction::create([
+            'category_id' => $this->categoryIncome->id,
+            'description' => 'transaction3',
+            'admin_id'    => $this->admin->id,
+            'amount'      => 7000,
+            'date'        => $this->transactionDay2
+        ]);
+    }
+
 }
