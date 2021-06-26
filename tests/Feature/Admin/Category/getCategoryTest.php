66ab1a4bc1f343aabf7e43c0137a3259d40cdfd8
rye gaspar <gaspar.ryan@outlook.com>
2021-06-25 21:04:30 -0500

    categories - index - add sort by name

--- tests/Feature/Admin/Category/getCategoryTest.php
+++ tests/Feature/Admin/Category/getCategoryTest.php
@@ -11,85 +11,85 @@ class getCategoryTest extends TestCase
     use RefreshDatabase;
 
     private $defaultValues = [
-            [
-                'id' => 1,
-                'type' => 'income',
-                'name' => 'sales',
-                'icon' => 'piggy-bank',
-            ],
-            [
-                'id' => 2,
-                'type' => 'income',
-                'name' => 'carry over',
-                'icon' => 'money-bill-wave',
-            ],
-            [
-                'id' => 3,
-                'type' => 'expense',
-                'name' => 'cash advance',
-                'icon' => 'coins',
-            ],
-            [
-                'id' => 4,
-                'type' => 'expense',
-                'name' => 'employee salary',
-                'icon' => 'credit-card',
-            ],
-            [
-                'id' => 5,
-                'type' => 'expense',
-                'name' => 'electric bill',
-                'icon' => 'lightbulb',
-            ],
-            [
-                'id' => 6,
-                'type' => 'expense',
-                'name' => 'internet',
-                'icon' => 'wifi',
-            ],
-            [
-                'id' => 7,
-                'type' => 'expense',
-                'name' => 'water',
-                'icon' => 'water',
+        [
+            'id'   => 1,
+            'type' => 'income',
+            'name' => 'sales',
+            'icon' => 'piggy-bank',
+        ],
+        [
+            'id'   => 2,
+            'type' => 'income',
+            'name' => 'carry over',
+            'icon' => 'money-bill-wave',
+        ],
+        [
+            'id'   => 3,
+            'type' => 'expense',
+            'name' => 'cash advance',
+            'icon' => 'coins',
+        ],
+        [
+            'id'   => 4,
+            'type' => 'expense',
+            'name' => 'employee salary',
+            'icon' => 'credit-card',
+        ],
+        [
+            'id'   => 5,
+            'type' => 'expense',
+            'name' => 'electric bill',
+            'icon' => 'lightbulb',
+        ],
+        [
+            'id'   => 6,
+            'type' => 'expense',
+            'name' => 'internet',
+            'icon' => 'wifi',
+        ],
+        [
+            'id'   => 7,
+            'type' => 'expense',
+            'name' => 'water',
+            'icon' => 'water',
 
-            ],
-            [
-                'id' => 8,
-                'type' => 'expense',
-                'name' => 'fuel',
-                'icon' => 'gas-pump',
-            ],
-            [
-                'id' => 9,
-                'type' => 'expense',
-                'name' => 'gas',
-                'icon' => 'fire',
-            ],
-            [
-                'id' => 10,
-                'type' => 'expense',
-                'name' => 'food',
-                'icon' => 'utensils',
-            ],
-            [
-                'id' => 11,
-                'type' => 'expense',
-                'name' => 'entertainment',
-                'icon' => 'tv',
-            ],
-            [
-                'id' => 12,
-                'type' => 'expense',
-                'name' => 'grocery',
-                'icon' => 'shopping-basket',
-            ],
-            [
-                'id' => 13,
-                'type' => 'expense',
-                'name' => 'equipment/tools',
-                'icon' => 'tools',
-            ]
+        ],
+        [
+            'id'   => 8,
+            'type' => 'expense',
+            'name' => 'fuel',
+            'icon' => 'gas-pump',
+        ],
+        [
+            'id'   => 9,
+            'type' => 'expense',
+            'name' => 'gas',
+            'icon' => 'fire',
+        ],
+        [
+            'id'   => 10,
+            'type' => 'expense',
+            'name' => 'food',
+            'icon' => 'utensils',
+        ],
+        [
+            'id'   => 11,
+            'type' => 'expense',
+            'name' => 'entertainment',
+            'icon' => 'tv',
+        ],
+        [
+            'id'   => 12,
+            'type' => 'expense',
+            'name' => 'grocery',
+            'icon' => 'shopping-basket',
+        ],
+        [
+            'id'   => 13,
+            'type' => 'expense',
+            'name' => 'equipment/tools',
+            'icon' => 'tools',
+        ]
     ];
 
     /** @test */
@@ -157,6 +157,36 @@ class getCategoryTest extends TestCase
                 'from',
                 'to',
                 'data',
+            ]);
+    }
+
+    /** @test */
+    public function can_sorted_by_name()
+    {
+        $admin = Admin::factory()->create();
+
+        $this->actingAs($admin, 'admin')
+            ->withHeaders(['accept' => 'application/json'])
+            ->getJson('admin/categories?sort=name|asc')
+            ->assertJson([
+                'data' => [
+                    [
+                        'id'     => 2,
+                        'name' => "carry over",
+                    ],
+                    [
+                        'id'     => 3,
+                        'name' => "cash advance",
+                    ],
+                    [
+                        'id' => 5,
+                        'name' => 'electric bill'
+                    ],
+                    [
+                        'id'     => 4,
+                        'name' => "employee salary",
+                    ],
+                ]
             ]);
     }
 }
