3c1a2021f9e94f58b3e804a3da27fde262688381
rye gaspar <gaspar.ryan@outlook.com>
2021-09-01 14:15:36 -0500

    fix category seeding

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -15,13 +15,11 @@ class TransactionFactory extends Factory
 
     public function definition()
     {
-        $category = Category::factory()->create();
-
         return [
-            'category_id' => $category->id,
+            'category_id' => Category::factory(),
             'admin_id'    => Admin::factory(),
             'account_id'  => Account::factory(),
-            'amount'      => 10000 * ($category->type === 'in' ? 1 : -1),
+            'amount'      => 10000,
             'date'        => Carbon::now(),
         ];
     }
